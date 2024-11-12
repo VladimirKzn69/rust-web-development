6f124fa07fdb65d7193dd35cdb2eb7618851f631
Vladimir Efimishchev <b2sh69@gmail.com>
2024-11-12 18:24:12 +0300

    create a product

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+/*
 use shoe_store::models::{create_product, NewProduct};
 
 #[cfg(test)]
@@ -23,6 +24,102 @@ fn create_product_test() {
             conn,
         );
         assert_eq!(Ok(1), results);
+        Ok(())
+    });
+}
+*/
+
+use diesel::prelude::*;
+use diesel::RunQueryDsl;
+use diesel::sqlite::SqliteConnection;
+
+use serde::{Serialize, Deserialize};
+use shoe_store::schema::products; // Подключение к схеме
+// pub use serde_derive::{Deserialize, Serialize};
+
+#[derive(Queryable, Debug)]
+#[derive(serde::Serialize, Deserialize)]
+pub struct Product {
+    pub id: Option<i32>,
+    pub name: String,
+    pub cost: f64,
+    pub active: bool,
+}
+
+pub fn list_products(conn: &mut SqliteConnection) -> Result<Vec<Product>, diesel::result::Error> {
+    // Загружаем все продукты из таблицы
+    products::table.load::<Product>(conn)
+}
+
+
+use ::shoe_store::establish_connection_test;
+use diesel::result::Error;
+
+#[test]
+
+fn create_product_test() {
+    use diesel::Connection;
+    use shoe_store::models::create_product;
+    use shoe_store::models::NewCompleteProduct;
+    use shoe_store::models::NewProduct;
+    use shoe_store::models::NewVariantValue;
+    use shoe_store::models::NewVariant;
+
+    let mut connection = establish_connection_test();
+    connection.test_transaction::<_, Error, _>(|conn| {
+        create_product(NewCompleteProduct {
+            product: NewProduct {
+                name: "boots".to_string(),
+                cost: 13.23,
+                active: true
+            },
+            variants: vec![
+                NewVariantValue {
+                    variant: NewVariant {
+                        name: "size".to_string()
+                    },
+                    values: vec![
+                        Some(12.to_string()),
+                        Some(14.to_string()),
+                        Some(16.to_string()),
+                        Some(18.to_string())
+                    ]
+                }
+            ]
+        }, conn).unwrap();
+
+        assert_eq!(
+            serde_json::to_string(&list_products(conn).unwrap()).unwrap(),
+            serde_json::to_string(&vec![
+                (
+                    Product {
+                        id: Some(1),
+                        name: "boots".to_string(),
+                        cost: 13.23,
+                        active: true
+                    },
+                    vec![
+                        (
+                            Some(12.to_string()),
+                            "size".to_string()
+                        ),
+                        (
+                            Some(14.to_string()),
+                            "size".to_string()
+                        ),
+                        (
+                            Some(16.to_string()),
+                            "size".to_string()
+                        ),
+                        (
+                            Some(18.to_string()),
+                            "size".to_string()
+                        )
+                    ]
+                ),
+            ]).unwrap()
+        );
+
         Ok(())
     });
 }
--- src/models.rs
+++ src/models.rs
@@ -1,5 +1,35 @@
-use diesel::Insertable;
+// use diesel::Insertable;
+extern crate diesel;
+
 use crate::schema::*;
+use diesel::{Insertable, Identifiable, Queryable};
+use serde::{Serialize, Deserialize};
+// use shoe_store::schema::variants;
+
+#[macro_use]
+// extern crate diesel;
+
+use anyhow::Result;
+use diesel::sqlite::SqliteConnection;
+use diesel::ExpressionMethods;
+// use ::shoe_store::models::*;
+use diesel::Connection;
+use diesel::RunQueryDsl;
+use diesel::query_dsl::QueryDsl;
+use crate::schema::products_variants;
+
+/*/
+use diesel::no_arg_sql_function;
+no_arg_sql_function!(last_insert_rowid, diesel::sql_types::Integer);
+*/
+use diesel::define_sql_function;
+use diesel::sql_types::Integer;
+
+define_sql_function!{
+    fn last_insert_rowid() -> Integer;
+}
+
+
 
 #[derive(Insertable, Debug)]
 #[diesel(table_name=products)]
@@ -8,15 +38,100 @@ pub struct NewProduct {
     pub cost: f64,
     pub active: bool,
 }
+
+// #[derive(Identifiable, Queryable, Debug, Serialize, Deserialize)]
+#[derive(Identifiable, Queryable, Debug)]
+#[table_name = "variants"]
+pub struct Variant {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Insertable, Debug, Clone)]
+#[table_name="variants"]
+pub struct NewVariant {
+    pub name: String,
+}
+
+// use shoe_store::schema::products_variants;
+
+#[derive(Insertable, Debug)]
+#[table_name="products_variants"]
+pub struct NewProductVariant {
+    pub product_id: i32,
+    pub variant_id: i32,
+    pub value: Option<String>
+}
+#[derive(Clone)]
+pub struct NewVariantValue {
+    pub variant: NewVariant,
+    pub values: Vec<Option<String>>
+}
+pub struct NewCompleteProduct {
+    pub product: NewProduct,
+    pub variants: Vec<NewVariantValue>
+}
+
 // use super::models::NewProduct;
+/*
 use diesel::sqlite::SqliteConnection;
 use diesel::result::Error;
 use diesel::RunQueryDsl;
+*/
 
-pub fn create_product(new_product: NewProduct, conn: &mut SqliteConnection) -> Result<usize, Error>  {
+/* pub fn create_product(new_product: NewProduct, conn: &mut SqliteConnection) -> Result<usize, Error>  {
     use crate::schema::products::dsl::*;
     // use crate::models::products::dsl::products;
     diesel::insert_into(products)
         .values(new_product)
         .execute(conn)
+}
+*/
+pub fn create_product(new_product: NewCompleteProduct, conn: &mut SqliteConnection) -> Result<i32>  {
+    use crate::schema::products::dsl::products;
+    use crate::schema::variants::dsl::*;
+    use crate::schema::products_variants::dsl::*;
+    use diesel::Connection;
+    use anyhow::Result;
+
+    conn.transaction(|conn| {
+        diesel::insert_into(products)
+            .values(new_product.product)
+            .execute(conn)?;
+
+        let last_product_id: i32 = diesel::select(last_insert_rowid()).first(conn)?;
+
+        for new_variant in new_product.variants {
+            let variants_result =
+                variants
+                    .filter(name.eq(&new_variant.variant.name))
+                    .limit(1)
+                    .load::<Variant>(conn)?;
+
+            let last_variant_id: i32 =
+                match variants_result.first() {
+                    Some(variant) => variant.id,
+                    None => {
+                        diesel::insert_into(variants)
+                            .values(name.eq(&new_variant.variant.name))
+                            .execute(conn)?;
+
+                        diesel::select(last_insert_rowid()).first(conn)?
+                    }
+                };
+
+            for new_value in new_variant.values {
+                diesel::insert_into(products_variants)
+                    .values(
+                        (
+                            product_id.eq(last_product_id), 
+                            variant_id.eq(last_variant_id),
+                            value.eq(new_value), 
+                        )
+                    )
+                    .execute(conn)?;
+            }
+        }
+        Ok(last_product_id)
+    })
 }
